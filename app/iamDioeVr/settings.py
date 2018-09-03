e117f20a4acfd6939e9ca5269e415855dc20edaa
hcb79 <hcb@hcb.de>
2018-09-03 07:52:11 +0200

    Init 4

--- app/iamDioeVr/settings.py
+++ app/iamDioeVr/settings.py
@@ -72,11 +72,7 @@ MIDDLEWARE_CLASSES = (
 	'django.contrib.messages.middleware.MessageMiddleware',
 	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 	'django.middleware.security.SecurityMiddleware',
-	'corsheaders.middleware.CorsMiddleware',
-	'django.middleware.common.CommonMiddleware',
 )
-
-CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'iamDioeVr.urls'
 
