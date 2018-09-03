b723af134b9d2a15bcf5861d67371d9e6fe800d4
hcb79 <hcb@hcb.de>
2018-09-03 09:11:00 +0200

    Seiten vorbereitet.

--- webpack_src/vr/src/App.vue
+++ webpack_src/vr/src/App.vue
@@ -1,21 +1,43 @@
 <template>
 	<div id="app">
-		<StartPage/>
+		<div class="content">
+			<StartPage @start="start()" v-if="site === 0"/>
+			<DataPage v-if="site === 1"/>
+		</div>
 	</div>
 </template>
 
 <script>
 	/* global csrf */
 	import StartPage from './components/StartPage'
+	import DataPage from './components/DataPage'
 
 	export default {
 		name: 'App',
+		http: {
+			root: '/data',
+			headers: {
+				'X-CSRFToken': csrf
+			},
+			emulateJSON: true
+		},
+		data () {
+			return {
+				site: 0,
+				playerId: 0,
+			}
+		},
 		mounted () {
-			console.log(csrf)
+		},
+		methods: {
+			start () {		// Wenn noch keine playerId dann zum Fragebogen für Sozialdaten, sonst weiter zum Spiel ...
+				this.site = ((this.playerId === 0) ? 1 : 2)
+			},
 		},
 		components: {
-			StartPage
-		}
+			StartPage,
+			DataPage,
+		},
 	}
 </script>
 
--- webpack_src/vr/src/components/DataPage.vue
+++ webpack_src/vr/src/components/DataPage.vue
@@ -1,14 +1,14 @@
 <template>
 	<div>
-		<h1>Startseite</h1>
+		<h1>Sozialdaten</h1>
 		<p>...</p>
-		<button @click="$emit('start')" type="button" class="btn btn-primary">Starten ...</button>
+		<!-- <button @click="$emit('start')" type="button" class="btn btn-primary">Starten ...</button> -->
 	</div>
 </template>
 
 <script>
 export default {
-	name: 'StartPage',
+	name: 'DataPage',
 	data () {
 		return {
 		}
